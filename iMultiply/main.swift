a708239078bfaa512eb8c8febda9111f2e98b028
Benjamin Hall <b.e.s.hall@outlook.com>
2019-02-10 18:21:28 +0000

    Added basic logic to repeat-while loop in main

--- iMultiply/main.swift
+++ iMultiply/main.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-enum QuestionType {
+enum QuestionType: CaseIterable {
     case add
     case subtract
     case multiply
@@ -22,6 +22,47 @@ class iMultiply {
         print("Welcome to iMultiple!")
         
         repeat {
+            let left = Int.random(in: 1...12)
+            let right = Int.random(in: 1...12)
+            let operation = QuestionType.allCases.randomElement()!
+            
+            let question: String
+            
+            switch operation {
+            case .add:
+                question = "What is \(left) plus \(right)?"
+            case .subtract:
+                question = "What is \(left) minus \(right)?"
+            case .multiply:
+                question = "What is \(left) multiplied by \(right)?"
+            }
+            
+            let correctAnswer: Int
+            
+            switch operation {
+            case .add: correctAnswer = left + right
+            case .subtract: correctAnswer = left - right
+            case .multiply: correctAnswer = left * right
+            }
+            
+            print("\n\(questionNumber). \(question)")
+            print("Your answer: ", terminator: "")
+            
+            if let answer = readLine() {
+                guard let answerInt = Int(answer) else {
+                    print("Error")
+                    continue
+                }
+                
+                questionNumber += 1
+                
+                if answerInt == correctAnswer {
+                    score += 1
+                    print("Correct")
+                } else {
+                    print("Wrong!")
+                }
+            }
             
         } while questionNumber < 10
         
